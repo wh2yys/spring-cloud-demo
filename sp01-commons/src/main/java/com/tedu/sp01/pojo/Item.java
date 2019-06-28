abe1d29ec7ba8deb971d9846b7d247f33511540b
wh2yys <wh2yys@126.com>
2019-06-28 10:11:15 +0800

    sp01-common

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,21 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @program: spring-cloud
+ * @author: Mr.WH
+ * @create: 2019-06-25 10:20
+ * @description:
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private Integer id;
+    private String name;
+    private Integer number;
+}
